b838af36fbccc54a817d930b82fb7495c6acaec5
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-06 14:25:26 +0100

    ADD: created the document

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// console.log('ciao mamma')
+
+//scrivi un programma che stampi in console i numeri da 1 a 100
+    //creo un ciclo for per creare i miei numeri da 1 a 100
+    //stampo il risultato del mio ciclo
+
+    //SE stampi i multipli di 3 stamperai "fizz" al posto del numero
+        //come faiccio a sapere se un numero e divisibile per 3?
+        //nomino una variabile e uso l' operatore modulo 
+        //stampo il valore della variabile 
+
+    //SE stampi i multipli di 5 stamperai "buzz" al posto del numero 
+        //come faiccio a sapere se un numero e divisibile per 5?
+        //nomino una variabile e uso l' operatore modulo 
+        //stampo il valore della variabile 
+        
+    //ALTRIMENTI SE sono multipli sia di 3 che di 5 stamperai "fizzbuzz"
