41dbb9625ffc5f0cf37fa865de385117bfa2794f
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-06 15:07:08 +0100

    Refact:comments

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 // console.log('ciao mamma')
 
 //scrivi un programma che stampi in console i numeri da 1 a 100
-    for(let i = 0; i < 100; i++ ){
-        let num = i + 1;
-        console.log(num)
-    }
     //creo un ciclo for per creare i miei numeri da 1 a 100
-    //stampo il risultato del mio ciclo
+    for(let i = 0; i < 100; i++ ){
+        //creo una variabile per visualizzare i numeri da 1 a 100 e non da 0 a 99
+        let num = i + 1;
+        //stampo il risultato del mio ciclo
 
-    //SE stampi i multipli di 3 stamperai "fizz" al posto del numero
-        //come faiccio a sapere se un numero e divisibile per 3?
+        console.log(num)
+
+        //SE stampi i multipli di 3 stamperai "fizz" al posto del numero
+            //come faiccio a sapere se un numero e divisibile per 3?
+        let numDivisoTre = num % 3;
+        if(numDivisoTre === 0){
+            console.log('Fizz')
+        }
+
+    }
+
         //nomino una variabile e uso l' operatore modulo 
         //stampo il valore della variabile 
 
