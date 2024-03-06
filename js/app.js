dd8e62aff920598906ebbdfb0b86c3fbe01c02ed
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-06 16:51:15 +0100

    ADD:tring to print the element inside the dom

--- js/app.js
+++ js/app.js
@@ -1,44 +1,40 @@
 // console.log('ciao mamma')
 
 //scrivi un programma che stampi in console i numeri da 1 a 100
-    //creo un ciclo for per creare i miei numeri da 1 a 100
-    for(let i = 0; i < 100; i++ ){
-        //creo una variabile per visualizzare i numeri da 1 a 100 e non da 0 a 99
-        let num = i + 1;
-        //stampo il risultato del mio ciclo
+//creo un ciclo for per creare i miei numeri da 1 a 100
+for (let i = 0; i < 100; i++) {
+    //creo una variabile per visualizzare i numeri da 1 a 100 e non da 0 a 99
+    let num = i + 1;
+    //stampo il risultato del mio ciclo
 
-        // console.log(num)
+    // console.log(num)
 
-        //SE stampi i multipli di 3 stamperai "fizz" al posto del numero
-            //come faiccio a sapere se un numero e divisibile per 3?
-        let numDivisoTre = num % 3;
+    //SE stampi i multipli di 3 stamperai "fizz" al posto del numero
+    //come faiccio a sapere se un numero e divisibile per 3?
 
-        //SE stampi i multipli di 5 stamperai "buzz" al posto del numero 
-            //come faiccio a sapere se un numero e divisibile per 5?
-        let numDivisoreCinque = num % 5;
+    let numDivisoTre = num % 3;
+    let numDivisoreCinque = num % 5;
 
-
-        if(numDivisoTre != 0){
-            console.log(num)
-        }else if(numDivisoTre === 0){
-            console.log('fizz')
-
-        } //else(numDivisoreCinque != 0){
-        //     console.log('fizz')
-        // }
+    
 
 
-        // if(numDivisoreCinque != 0){
-        //     console.log(num)
-        // } else(numDivisoreCinque === 0){
-        //     console.log('buzz')
-        // }
+    
+    if(numDivisoTre === 0 && numDivisoreCinque === 0) { // SE è multiplo di 3 e di 5
+        //Stampo "fizzbuzz"
+        console.log('fizzbuzz');
+
+    }else if (numDivisoTre === 0) { // SE il numero è multiplo di 3
+        // Stampo Fizz
+        console.log('fizz');
+
+    } else if(numDivisoreCinque === 0) { //ALTRIMENTI SE multiplo di 5
+        //Stampo buzz
+        console.log('buzz');
+
+    } else{
+        console.log(num);
     }
+    
+}
 
-        //nomino una variabile e uso l' operatore modulo 
-        //stampo il valore della variabile 
-
-        //nomino una variabile e uso l' operatore modulo 
-        //stampo il valore della variabile 
-
-    //ALTRIMENTI SE sono multipli sia di 3 che di 5 stamperai "fizzbuzz"
+//chiamo il mio container dal DOM
